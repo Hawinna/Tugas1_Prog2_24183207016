6788428fd1ff92231e32e77200aa4c5d4580f7ce
Hawinna <idzazia@gmail.com>
2025-10-19 22:02:58 +0700

    Add files via upload

---
+++ NewClass.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mahasiswa;
+
+/**
+ *
+ * @author hawinna
+ */
+public class NewClass {
+        public static void main (String[]args){
+        Mahasiswa mhs1=new Mahasiswa();
+        mhs1.nama("Ziano Albagus");
+        mhs1.npm("24183207015");
+        mhs1.jurusan("Informatika");
+        mhs1.ukm("badminton");
+        mhs1.tampil();
+        
+        Mahasiswa mhs2=new Mahasiswa();
+        mhs2.nama("Idza Zia");
+        mhs2.npm("24183207016");
+        mhs2.jurusan("Pendidikan Teknologi Informasi");
+        mhs2.ukm("Tari");
+        mhs2.tampil();
+        
+        }
+
+}
+
+
